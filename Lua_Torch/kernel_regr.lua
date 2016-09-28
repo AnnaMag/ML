206d410bf5bf94ba4d2f70bd1a1ac752a38dd137
AnnaMag <AnnaMag@users.noreply.github.com>
2016-09-28 21:55:45 +0200

    finish ex

--- Lua_Torch/kernel_regr.lua
+++ Lua_Torch/kernel_regr.lua
@@ -1,6 +1,8 @@
 require 'torch'
 require 'gnuplot'
 
+-- Kernel regression
+
 --data samples
 local nData = 10
 
@@ -11,9 +13,46 @@ local kWidth =1
 local xTrain = torch.linspace(-1,1,nData)
 
 --quadratic fun: y^2
-local xTrain = torch.pow(xTrain,2)
+local yTrain = torch.pow(xTrain,2)
 
 -- adding Gaussian noise ()
-local yTrain = yTrain + torch.mul(torch.randn(nData),0,1)
+local yTrain = yTrain + torch.mul(torch.randn(nData),0.1)
 
 --create kernel function
+local function phi(x, y)
+  return torch.exp(-(1/kWidth)*torch.sum(torch.pow(x-y,2)))
+end
+
+--
+local Phi = torch.Tensor(nData, nData)
+for i = 1,nData do
+  for j = 1,nData do
+    Phi[i][j] = phi(xTrain[{{i}}],xTrain[{{j}}])
+  end
+end
+
+-- delta = 0.001
+local regularizer = torch.mul(torch.eye(nData), 0.001)
+
+-- [Phi^TPhi + delta^2 * Id]^-1*Phi^T*Y
+local theta = torch.inverse((Phi:t()*Phi) + regularizer) * Phi:t() *yTrain
+print(Phi)
+print(theta)
+
+--test data
+local nTestData = 100
+local xTest = torch.linspace(-1,1,nTestData)
+
+local PhiTest = torch.Tensor(nData, nTestData)
+for i = 1,nData do
+  for j = 1,nTestData do
+    PhiTest[i][j] = phi(xTrain[{{i}}],xTest[{{j}}])
+  end
+end
+
+--prediction
+local yPred = PhiTest:t() * theta
+
+gnuplot.plot({'Data', xTrain, yTrain,'+'}, {'Prediction', xTest, yPred, '-'})
+
+--print(yPred)
